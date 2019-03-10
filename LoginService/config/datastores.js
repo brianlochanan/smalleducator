c9393c0e8008a06f873f9994dc1808a3d891ed38
Unknown <firebear14@gmail.com>
2019-03-11 00:23:40 +0100

    share mysql container with docker-compose

--- LoginService/config/datastores.js
+++ LoginService/config/datastores.js
@@ -49,7 +49,7 @@ module.exports.datastores = {
     *                                                                          *
     ***************************************************************************/
     adapter: 'sails-mysql',
-    url: 'mysql://user:password@host:port/database',
+    url: 'mysql://root:root@172.32.0.1:9003/sails',
 
   },
 
