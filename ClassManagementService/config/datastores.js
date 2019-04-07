3f08805b93350d40b097db1474bd912d453f4bdb
unknown <firebear14@gmail.com>
2019-04-07 21:45:54 +0200

    Setup db

--- ClassManagementService/config/datastores.js
+++ ClassManagementService/config/datastores.js
@@ -48,8 +48,8 @@ module.exports.datastores = {
     *    (See https://sailsjs.com/config/datastores for help.)                 *
     *                                                                          *
     ***************************************************************************/
-    // adapter: 'sails-mysql',
-    // url: 'mysql://user:password@host:port/database',
+    adapter: 'sails-mysql',
+    url: `mysql://root:root@${process.env.HOST_IP}:9008/sails`,
 
   },
 
